00adee61c799b64fd7f55b87b1a500ba4c7c34f7
whisping <whisping@gmail.com>
2018-10-19 03:57:14 +0300

    intro to segues

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 
     
 
